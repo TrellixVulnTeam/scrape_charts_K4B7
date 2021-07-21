2dae1adcadc58fc927cf7414720c75f21a57b5b9
Pranit Shah <ppshah2023@email.medfield.net>
2021-07-21 16:42:19 -0400

    Attempt One
    Not working, attempt one at a fix

--- src/chart_scraper/ChartScraper.py
+++ src/chart_scraper/ChartScraper.py
@@ -3,7 +3,7 @@ import requests
 import re
 import pandas as pd
 
-class ScrapeCharts:
+class Scraper:
     """
         Please note: each website should remain in string format
         This class will guide you through the process with print statements
@@ -15,18 +15,20 @@ class ScrapeCharts:
         Please note: each website should remain in string format
         """
         self.listedCharts = []
-        if websites is str:
+        if type(websites) is str:
             self.websites = [websites]
             print("(scraper) Single website saved as list")
             print(self.websites)
         else:
-            self.websites = list(websites)
+            self.websites = websites
             print("(scraper) Websites saved as list")
             print(self.websites)
         for eachSite in self.websites:
-            self.listedCharts.append(self.getTables(self.scrapeSite(eachSite)))
+            self.getTables(self.scrapeSite(eachSite))
+        for eachRawChart in self.rawCharts:
+            self.listedCharts.append(self.makelist(eachRawChart))
         print("(scraper) time to choose which charts to keep")
-        self.displayTables(self.listedCharts)
+        self.displayTables()
         
             
     def scrapeSite(self, url: list):
@@ -35,8 +37,8 @@ class ScrapeCharts:
         :param url: list of urls or a single url, all urls are in string format
         :return: htmldoc of each item
         """
-        toReturn = requests.get(url).content
-        print("(scraper) Found" + url)
+        toReturn = requests.get(url).text
+        print("(scraper) Found " + url)
         return toReturn
 
     def getTables(self, htmldoc):
@@ -45,33 +47,49 @@ class ScrapeCharts:
         :param htmldoc: list of or one of a htmldoc
         :return: tables as a list
         """
-        soup = BeautifulSoup(htmldoc, features="html.parser")
-        return self.makelist(soup.findAll('table'))
+        soup = BeautifulSoup(htmldoc, features="lxml")
+        print("(scraper) BeautifulSoup has parsed the website")
+        self.rawCharts = list()
+        self.rawCharts = self.rawCharts + soup.find_all('table')
+        print(self.rawCharts)
 
     def makelist(self, table):
-        """
-        Print all tables so that you can pick the tables you want (not my code, but still reliable)
-        :param listOfTables: list of or one of a htmldoc
-        :return: tables as pandas' DataFrame
-        """
-        result = []
-        allrows = table.findAll('tr')
-        for row in allrows:
-            result.append([])
-            allcols = row.findAll('td')
-        for col in allcols:
-            thestrings = [str(s) for s in col.findAll(text=True)]
-            thetext = ''.join(thestrings)
-            result[-1].append(thetext)
-        return result
-
-    def displayTables(self, listOfTables):
         """
         Print all tables so that you can pick the tables you want
         :param listOfTables: list of or one of a htmldoc
         :return: tables as pandas' DataFrame
         """
-        df = pd.DataFrame(listOfTables)
+        try:
+            rows = table.find_all('tr')
+            if bool(rows):
+                for i in rows:
+                    table_data = i.find_all('td')
+                    data = [j.text for j in table_data]
+                    print(data)
+            # result = []
+            # allrows = []
+            # for row in table:
+            #     allrows.append(table.find_all('tr'))
+            # allcols = []
+            # for row in allrows:
+            #     result.append([])
+            #     allcols = allcols + row.find_all('td')
+            # for col in allcols:
+            #     thestrings = [str(s) for s in col.find_all(text=True)]
+            #     thetext = ''.join(thestrings)
+            #     result[-1].append(thetext)
+            # return result
+        except AttributeError:
+            print("(scraper) Possibly something went wrong and the results won't match what you expect")
+            return []
+
+    def displayTables(self):
+        """
+        Print all tables so that you can pick the tables you want
+        :param listOfTables: list of or one of a htmldoc
+        :return: tables as pandas' DataFrame
+        """
+        df = pd.DataFrame(self.listedCharts)
         print(df)
         print("(scraper) Remove unnecsary charts through removing items from the list Scraper.listedCharts")
         print("(scraper) Once done, call Scraper.cleanList with your preferred arguments to preview cleaning")
@@ -132,22 +150,56 @@ class ScrapeCharts:
         self.listedCharts = processedList
         self.displayTables()
         print("(scraper) Many prefer to turn this list into a dictionary")
-        print("(scraper) If you would like this, call scraper.listToDict")
+        print("(scraper) If you would like this, call Scraper.listToDict()")
     
-    def listToDict(self):
-        z = {}
-        for eachrow in self.listedCharts:
+    def listToDict(self, indexList=[0, 1, 2], keyAsList=False, includePrintStatement=True):
+        """
+        This functions converts the saved class variable of the list into a dictionary with the first index as the key and the other indexes in a list. You can change the order by passing in a value for the default.
+        :list indexList: this is the order of the indexes used
+        :bool keyAsList: should this function convert the key from a list into a single element
+        :bool includePrintStatement: prints out "(scraper) You can now get the keys of this dictionary by calling Scraper.getDictKeys()"
+        :return: dictionary
+        """
+        self.dictionariedChart = dict()
+        for eachRow in self.listedCharts:
             try:
-                # The index may change for the examples, but usually it's either 2 or 3
-                word = eachrow[0]
-                definition = eachrow[1]
-                examples = eachrow[2]
-                for eachword in word:
-                    processedExamples = []
-                    for eachexample in examples:
-                        if eachword in eachexample:
-                            processedExamples.append(eachexample) 
-                    z.update({eachword: [definition, processedExamples]})
+                reorderedList = []
+                for eachIndex in indexList:
+                    reorderedList.append(eachRow[eachIndex])
+                key = reorderedList[0]
+                value = reorderedList.pop(0)
+                if keyAsList:
+                    self.dictionariedChart.update({key: value})
+                else:
+                    for eachItem in key:
+                        self.dictionariedChart.update({eachItem: value})
             except IndexError:
                 pass
-        return z
+        if includePrintStatement:
+            print("(scraper) You can now get the keys of this dictionary by calling Scraper.getDictKeys()")
+        return self.dictionariedChart
+    
+    def getDictKeys(self, includePrintStatement=True):
+        """
+        This functions gets all the keys from the dictionary
+        :bool includePrintStatement: prints out "(scraper) You can get the keys by calling Scraper.dictKeys or saving the return statement"
+        :return: list of keys
+        """
+        self.dictKeys = dict(self.dictionariedChart).keys()
+        if includePrintStatement:
+            print("(scraper) You can get the keys by calling Scraper.dictKeys or saving the return statement")
+        return self.dictKeys
+
+    def findWordComponents(self, testWord):
+        """
+        This functions finds any key in the dictionary that is a component of the testWord. If you don't want a comprehensive search, just do Scraper.
+        :param test: usually a string, however, other options will work
+        :return: dictionary with keys and values that worked
+        """
+        components = self.dictKeys
+        foundKey = [eachComponent for eachComponent in components if eachComponent in testWord]
+        foundData = {}
+        for eachKey in foundKey:
+            foundData.update({eachKey: components[eachKey]})
+        print(foundData)
+        return foundData
---
+++ tests/test_scraper.py
@@ -0,0 +1,11 @@
+from src.chart_scraper.ChartScraper import Scraper
+
+
+# This is for educational purposes only, the fact that there is no assert statements support this idea
+chartScraper = Scraper("https://www.learnthat.org/pages/view/roots.html")
+chartScraper.listedCharts = chartScraper.listedCharts[0]
+chartScraper.cleanList()
+chartScraper.listToDict(indexList=[0,1,3])
+chartScraper.listToDict()
+chartScraper.getDictKeys()
+print(chartScraper.findWordComponents("Philology"))
